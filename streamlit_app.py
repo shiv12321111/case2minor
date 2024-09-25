ee18ba18eacb1dbb8fd1975eceb909eabbb0902f
shiv12321111 <shivanokalka@gmail.com>
2024-09-25 12:28:08 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Openmeteo Weather App",
+)
+
+st.sidebar.success("Select a demo above.")
+
+st.title('Openmeteo Weather app')
+
+st.info('This is a weather app')
